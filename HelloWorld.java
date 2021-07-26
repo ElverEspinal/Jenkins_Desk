cd4093c14e3675008af70698f83769afcae3c987
ElverEspinal <87837818+ElverEspinal@users.noreply.github.com>
2021-07-26 10:17:16 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
- import javax.swing.JOptionPane;
-    
+
     public class HelloWorld {
         public static void main(String[] args) {
-            JOptionPane.showMessageDialog(null, "Hello World");
+            System.out.println(" Ejecución exitosa, Hola Mundo");;
         }
     }
